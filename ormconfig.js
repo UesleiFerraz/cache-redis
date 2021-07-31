28eb23572442effe817c9f46d2c1c27d014c4e95
UesleiFerraz <uesleivaleriod@gmail.com>
2021-07-30 21:07:31 -0300

    project started

---
+++ ormconfig.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+module.exports = {
+  type: process.env.DB_DIALECT,
+  url: process.env.DB_URL,
+  synchronize: false,
+  logging: true,
+  entities: [`${process.env.SOURCE_DB}/core/infra/data/database/entities/**/*`],
+  migrations: [
+    `${process.env.SOURCE_DB}/core/infra/data/database/migrations/**/*`,
+  ],
+  cli: {
+    entitiesDir: `${process.env.SOURCE_DB}/core/infra/data/database/entities`,
+    migrationsDir: `${process.env.SOURCE_DB}/core/infra/data/database/migrations`,
+  },
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+};
