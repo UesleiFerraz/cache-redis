189d917b5b9e09fe608223816a8941c6c125076e
UesleiFerraz <uesleivaleriod@gmail.com>
2021-07-31 23:14:36 -0300

    add: scrap routes

---
+++ src/features/scraps/presentation/routes/routes.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import {
+  EMVC,
+  middlewareAdapter,
+  MVCController,
+  routerMvcAdapter,
+} from "../../../../core/presentation";
+import { CacheRepository, ScrapRepository } from "../../infra";
+import { ScrapController } from "../controllers";
+import { ScrapMiddleware } from "../middlewares";
+
+const makeController = (): MVCController => {
+  const repository = new ScrapRepository();
+  const cache = new CacheRepository();
+
+  return new ScrapController(repository, cache);
+};
+
+export default class ScrapRoutes {
+  public init(routes: Router) {
+    routes.get("/scraps", routerMvcAdapter(makeController(), EMVC.INDEX));
+
+    routes.get("/scraps/:uid", routerMvcAdapter(makeController(), EMVC.SHOW));
+
+    routes.post(
+      "/scraps",
+      middlewareAdapter(new ScrapMiddleware()),
+      routerMvcAdapter(makeController(), EMVC.STORE)
+    );
+
+    routes.put(
+      "/scraps/:uid",
+      middlewareAdapter(new ScrapMiddleware()),
+      routerMvcAdapter(makeController(), EMVC.UPDATE)
+    );
+
+    routes.delete(
+      "/scraps/:uid",
+      routerMvcAdapter(makeController(), EMVC.DELETE)
+    );
+  }
+}
