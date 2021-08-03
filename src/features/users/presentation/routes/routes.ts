222d96b525ac3185e37c76c784ff8b63eca6b931
UesleiFerraz <uesleivaleriod@gmail.com>
2021-08-02 22:05:16 -0300

    add: user routes

---
+++ src/features/users/presentation/routes/routes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import {
+  EMVC,
+  middlewareAdapter,
+  routerAdapter,
+  routerMvcAdapter,
+} from "../../../scraps/presentation";
+import { UserRepository } from "../../infra";
+import { UserController } from "../controllers";
+import { UserFieldsMiddleware } from "../middlewares";
+
+const makeController = () => {
+  const repository = new UserRepository();
+
+  return new UserController(repository);
+};
+
+export default class UserRoutes {
+  public init(routes: Router): void {
+    routes.post(
+      "/users",
+      middlewareAdapter(new UserFieldsMiddleware()),
+      routerMvcAdapter(makeController(), EMVC.STORE)
+    );
+    routes.post(
+      "/auth",
+      middlewareAdapter(new UserFieldsMiddleware()),
+      routerAdapter(makeController())
+    );
+  }
+}
