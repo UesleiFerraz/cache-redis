83f49a7676d4514657139604ff90b247ca0ed88f
UesleiFerraz <uesleivaleriod@gmail.com>
2021-08-03 14:28:34 -0300

    add: try catch block on user controller

--- src/features/users/presentation/controllers/user.controller.ts
+++ src/features/users/presentation/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import {
   HttpResponse,
   MVCController,
   ok,
+  serverError,
 } from "../../../../core/presentation";
 import { UserRepository } from "../../infra";
 import bcrypt from "bcrypt";
@@ -14,6 +15,68 @@ export class UserController implements MVCController {
   constructor(repository: UserRepository) {
     this.#repository = repository;
   }
+
+  public async store(request: HttpRequest): Promise<HttpResponse> {
+    try {
+      const user = await this.#repository.getOne(request.body.username);
+
+      if (user) {
+        return {
+          statusCode: 409,
+          body: {
+            message: "Username already taken",
+          },
+        };
+      }
+
+      const newUser = await this.#repository.create(request.body);
+
+      return ok(newUser);
+    } catch (error) {
+      console.log(error);
+      return serverError();
+    }
+  }
+
+  public async handle(request: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { username, password } = request.body;
+
+      const user = await this.#repository.getOne(username);
+
+      if (!user) {
+        return {
+          statusCode: 404,
+          body: {
+            message: "User not found",
+          },
+        };
+      }
+
+      const secret = process.env.SECRET || "123";
+      let token: string | undefined;
+      if (user.password) {
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+          return {
+            statusCode: 401,
+            body: {
+              message: "Invalid password",
+            },
+          };
+        }
+
+        token = jwt.sign({ id: user.uid }, secret, { expiresIn: "1h" });
+      }
+
+      return ok(token);
+    } catch (error) {
+      console.log(error);
+      return serverError();
+    }
+  }
+
   index(request: HttpRequest): Promise<HttpResponse> {
     throw new Error("Method not implemented.");
   }
@@ -25,56 +88,5 @@ export class UserController implements MVCController {
   }
   delete(request: HttpRequest): Promise<HttpResponse> {
     throw new Error("Method not implemented.");
-  }
-
-  public async store(request: HttpRequest): Promise<HttpResponse> {
-    const user = await this.#repository.getOne(request.body.username);
-
-    if (user) {
-      return {
-        statusCode: 409,
-        body: {
-          message: "Username already taken",
-        },
-      };
-    }
-
-    const newUser = await this.#repository.create(request.body);
-
-    return ok(newUser);
-  }
-
-  public async handle(request: HttpRequest): Promise<HttpResponse> {
-    const { username, password } = request.body;
-
-    const user = await this.#repository.getOne(username);
-
-    if (!user) {
-      return {
-        statusCode: 404,
-        body: {
-          message: "User not found",
-        },
-      };
-    }
-
-    const secret = process.env.SECRET || "123";
-    let token: string | undefined;
-    if (user.password) {
-      const isValidPassword = await bcrypt.compare(password, user.password);
-
-      if (!isValidPassword) {
-        return {
-          statusCode: 401,
-          body: {
-            message: "Invalid password",
-          },
-        };
-      }
-
-      token = jwt.sign({ id: user.uid }, secret, { expiresIn: "1h" });
-    }
-
-    return ok(token);
   }
 }
