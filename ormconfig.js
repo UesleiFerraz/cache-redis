8ed06514f23ddf38d46b70f70250e17c465fd933
UesleiFerraz <uesleivaleriod@gmail.com>
2021-08-06 14:40:53 -0300

    fix to deploy app

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   type: process.env.DB_DIALECT,
-  url: process.env.DB_URL,
+  url: process.env.DATABASE_URL,
   synchronize: false,
   logging: true,
   entities: [`${process.env.SOURCE_DB}/core/infra/data/database/entities/**/*`],
